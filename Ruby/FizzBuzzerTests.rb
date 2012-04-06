8e189505657df959f16af6c9164670c02815eba3
pjvds <pj@born2code.net>
2012-04-06 15:41:43 +0200

    Adds failing test that assert for a fizz number from 1..100

--- Ruby/FizzBuzzerTests.rb
+++ Ruby/FizzBuzzerTests.rb
@@ -13,4 +13,12 @@ class FizzBuzzerTests < Test::Unit::TestCase
    def test_stringFor_returns_1_for_number_1
       assert_equal("1", @fb.stringFor(1))
    end
+
+   def test_stringFor_returns_fizz_for_every_number_divisible_by_three
+      divisibleByThree = (1..100).find_all {|i| i%3==0}
+      
+      divisibleByThree.each do |number|
+         assert_equal("fizz", @fb.stringFor(number))
+      end
+   end
 end
