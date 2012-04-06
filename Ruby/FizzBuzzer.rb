061a2ab2b4cce645796b78eb6a4701b2ea4d7770
pjvds <pj@born2code.net>
2012-04-06 16:19:40 +0200

    Refactor FizzBuzzer class to move readable code.

--- Ruby/FizzBuzzer.rb
+++ Ruby/FizzBuzzer.rb
@@ -2,18 +2,17 @@ class FizzBuzzer
 
    def stringFor(number)
       result=""
-      if number % 3  == 0
-         result += "fizz"
-      end
-      if number % 5 == 0
-         result += "buzz"
-      end
-
-      if(result=="")
-         result=number.to_s()
-      end
-
-      result
+      if isFizz(number) then result += "fizz" end
+      if isBuzz(number) then result += "buzz" end
+      
+      return (result=="") ? number.to_s() : result
    end
 
+   def isFizz(number)
+      (number % 3 == 0)
+   end
+
+   def isBuzz(number)
+      (number % 5 == 0)
+   end
 end
