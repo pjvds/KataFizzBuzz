8baa215589b7599658b628470cc5e353abefc0f4
pjvds <pj@born2code.net>
2012-04-06 14:50:42 +0200

    Adds failing test that assert that a non fizz number should be returned as number.

--- Ruby/FizzBuzzerTests.rb
+++ Ruby/FizzBuzzerTests.rb
@@ -2,10 +2,15 @@ require "FizzBuzzer"
 require "test/unit"
 
 class FizzBuzzerTests < Test::Unit::TestCase
-
-   def test_stringFor_returns_fizz_for_number_3
-      fb = FizzBuzzer.new()
-      assert_equal("fizz", fb.stringFor(3))
+   def setup
+      @fb = FizzBuzzer.new()
    end
 
+   def test_stringFor_returns_fizz_for_number_3
+      assert_equal("fizz", @fb.stringFor(3))
+   end
+
+   def test_stringFor_returns_1_for_number_1
+      assert_equal("1", @fb.stringFor(1))
+   end
 end
