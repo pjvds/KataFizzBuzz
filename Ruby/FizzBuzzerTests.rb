671eaea2c5ce1b08bc4dbeb45a76f4e3ff6e5751
pjvds <pj@born2code.net>
2012-04-06 14:10:37 +0200

    Adds first failing test that assert that 3 should be translated to 'fizz'

---
+++ Ruby/FizzBuzzerTests.rb
@@ -0,0 +1,11 @@
+require "FizzBuzzer"
+require "test/unit"
+
+class FizzBuzzerTests < Test::Unit::TestCase
+
+   def test_stringFor_returns_fizz_for_number_3
+      fb = FizzBuffer.new()
+      assert_equal("fizz", fb.stringFor(3))
+   end
+
+end
