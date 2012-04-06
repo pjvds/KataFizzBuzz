1addc8c41e39d318e8c6f69ff37d687a1185aadb
pjvds <pj@born2code.net>
2012-04-06 14:20:13 +0200

    Fix small typo in test.

--- Ruby/FizzBuzzerTests.rb
+++ Ruby/FizzBuzzerTests.rb
@@ -4,7 +4,7 @@ require "test/unit"
 class FizzBuzzerTests < Test::Unit::TestCase
 
    def test_stringFor_returns_fizz_for_number_3
-      fb = FizzBuffer.new()
+      fb = FizzBuzzer.new()
       assert_equal("fizz", fb.stringFor(3))
    end
 
