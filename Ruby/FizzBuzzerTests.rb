8c5df6f2bbd4f4d8cdb0c811b9eac61f30b2d934
pjvds <pj@born2code.net>
2012-04-06 16:22:38 +0200

    Refactor out some magic strings in FizzBuzzerTests

--- Ruby/FizzBuzzerTests.rb
+++ Ruby/FizzBuzzerTests.rb
@@ -2,16 +2,20 @@ require "FizzBuzzer"
 require "test/unit"
 
 class FizzBuzzerTests < Test::Unit::TestCase
+   @@fizz = "fizz"
+   @@buzz = "buzz"
+   @@fizzbuzz = @@fizz+@@buzz
+
    def setup
       @fb = FizzBuzzer.new()
    end
 
    def test_stringFor_returns_fizz_for_number_3
-      assert_equal("fizz", @fb.stringFor(3))
+      assert_equal(@@fizz, @fb.stringFor(3))
    end
 
    def test_stringFor_returns_buzz_for_number_5
-      assert_equal("buzz", @fb.stringFor(5))
+      assert_equal(@@buzz, @fb.stringFor(5))
    end
 
    def test_stringFor_returns_1_for_number_1
@@ -22,7 +26,7 @@ class FizzBuzzerTests < Test::Unit::TestCase
       divisibleByThree = (1..100).find_all {|i| i%3==0 && i%5!=0}
       
       divisibleByThree.each do |number|
-         assert_equal("fizz", @fb.stringFor(number))
+         assert_equal(@@fizz, @fb.stringFor(number))
       end
    end
 
@@ -30,7 +34,7 @@ class FizzBuzzerTests < Test::Unit::TestCase
       divisibleByFive = (1..100).find_all {|i| i%5==0 && i%3!=0}
       
       divisibleByFive.each do |number|
-         assert_equal("buzz", @fb.stringFor(number))
+         assert_equal(@@buzz, @fb.stringFor(number))
       end
    end
 
@@ -38,7 +42,7 @@ class FizzBuzzerTests < Test::Unit::TestCase
       divisibleByThreeAndFive = (1..100).find_all {|i| i%3==0 && i%5==0}
 
       divisibleByThreeAndFive.each do |number|
-         assert_equal("fizzbuzz", @fb.stringFor(number))
+         assert_equal(@@fizzbuzz, @fb.stringFor(number))
       end
    end
 end
